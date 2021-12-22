27f185f666d86e33b6a37c261cf6bafb814b5232
kato1971 <88402289+kato1971@users.noreply.github.com>
2021-12-22 14:24:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Part 1
+const age = 50;
+
+if (age >= 18) {
+    console.log("Jes you are old enough, you can come in!");
+}
+else {
+    console.log("Sorry you are to young, please stay out!");
+}
+
+//Part 2
+const isFemale = "True";
+
+if (isFemale == "True") {
+    console.log("You are Female!");
+}
+else {
+    console.log("you are Male!");
+}
+
+//Part 3
+const driverStatus = "alcohol";
+
+if (driverStatus == "bob") {
+    console.log("You are the bob, you are allowed to drive!");
+}
+else {
+    console.log("You drink alcohol, you are not allowed to drive!")
+}
